aa5c9957855b9ae856a1029dbf316e0eef5a91e4
Lateef Taiwo <taiwolateef55@gmail.com>
2024-02-19 11:40:49 +0100

    modified:   tooling.sh         modified:   wordpress.sh

--- tooling.sh
+++ tooling.sh
@@ -10,15 +10,22 @@ yum install -y php php-common php-mbstring php-opcache php-intl php-xml php-gd p
 systemctl start php-fpm
 systemctl enable php-fpm
 git clone https://github.com/Livingstone95/tooling-1.git
-mkdir /var/www/html
-cp -R /tooling-1/html/*  /var/www/html/
-cd /tooling-1
-mysql -h acs-database.cdqpbjkethv0.us-east-1.rds.amazonaws.com -u ACSadmin -p toolingdb < tooling-db.sql
-cd /var/www/html/
-touch healthstatus
-sed -i "s/$db = mysqli_connect('mysql.tooling.svc.cluster.local', 'admin', 'admin', 'tooling');/$db = mysqli_connect('acs-database.cdqpbjkethv0.us-east-1.rds.amazonaws.com ', 'ACSadmin', 'admin12345', 'toolingdb');/g" functions.php
+#mkdir /var/www/html
+sudo cp -rf tooling-1/html/*  /var/www/html/
+cd tooling-1
+mysql -h savvytek-database.c4scns6d3saq.eu-west-2.rds.amazonaws.com -u savvytekadmin -p toolingdb < tooling-db.sql
+sudo touch healthstatus
+sudo sed -i "s/$db = mysqli_connect('mysql.tooling.svc.cluster.local', 'admin', 'admin', 'tooling');/$db = mysqli_connect('savvytek-database.c4scns6d3saq.eu-west-2.rds.amazonaws.com', 'savvytekadmin', 'admin12345', 'toolingdb');/g" functions.php
 chcon -t httpd_sys_rw_content_t /var/www/html/ -R
-systemctl restart httpd
+sudo systemctl restart httpd
+sudo systemctl status httpd
+
+
+
+
+
+
+
 
 
 
